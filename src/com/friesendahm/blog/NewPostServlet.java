51bf9865dd551f8e3400affa2a7090d0f27703c4
Jonathan Friesen <jonny28f@gmail.com>
2016-09-17 15:04:42 -0500

    cleaned up NewPostSevlet

--- src/com/friesendahm/blog/NewPostServlet.java
+++ src/com/friesendahm/blog/NewPostServlet.java
@@ -2,10 +2,7 @@ package com.friesendahm.blog;
 
 import static com.googlecode.objectify.ObjectifyService.ofy; 
 import com.googlecode.objectify.ObjectifyService;
-
-
 import com.friesendahm.blog.BlogPost;
-
 import com.google.appengine.api.users.User;
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
@@ -24,41 +21,16 @@ public class NewPostServlet extends HttpServlet {
                 throws IOException {
         UserService userService = UserServiceFactory.getUserService();
         User user = userService.getCurrentUser();
-
- 
-
-        // We have one entity group per Guestbook with all Greetings residing
-
-        // in the same entity group as the Guestbook to which they belong.
-
-        // This lets us run a transactional ancestor query to retrieve all
-
-        // Greetings for a given Guestbook.  However, the write rate to each
-
-        // Guestbook should be limited to ~1/second.
-        
         String userName ="";
         if (user == null){
         	userName = "default";
         }else{
         	userName = user.getNickname();
         }
-
-
         String content = req.getParameter("content");
-
         Date date = new Date();
-
-
-        
-        ////Create a new post using the user and the content.
-        BlogPost post = new BlogPost(user,content);
-        ////Chuck the Greeting into Objectify using a synchronous call (see above).
-        ofy().save().entities(post).now();
-        ////Send the response (be sure to redirect the user to the ofyguestbook.jsp that you defined instead of guestbook.jsp).
-
-       resp.sendRedirect("/");
-
+        BlogPost post = new BlogPost(user,content);//Create a new post using the user and the content.
+        ofy().save().entities(post).now();//Chuck the BlogPost into Objectify using a synchronous call
+        resp.sendRedirect("/");//Send the response 
     }
-
 }
